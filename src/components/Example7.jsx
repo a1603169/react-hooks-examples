ac599b427b89c985b2032d01ea853a882a6973cb
seunghunbang <upi21x@gmail.com>
2021-07-17 19:15:31 +0300

    react-hooks-examples

---
+++ src/components/Example7.jsx
@@ -0,0 +1,37 @@
+import React, {useState, useMemo, useCallback} from 'react'
+
+function sum(persons) {
+  console.log("sum...")
+  return persons.map(person => person.age).reduce((l, r) => l + r, 0)
+}
+
+export default function Example7() {
+
+  const [value, setValue] = useState('')
+  const [persons] = useState([
+    {name: 'David', age: 24},
+    {name: 'Abby', age: 29},
+  ]);
+  
+  const count = useMemo(() => {
+    return sum(persons);
+  }, [persons])
+
+  const click = useCallback(() => {
+    console.log(value)
+  }, []);
+
+  
+
+  return (
+    <div>
+      <input value={value} onChange={change} />
+      <p>{count}</p>
+      <button onClick={click}>click</button>
+    </div>
+  )
+
+  function change(e) {
+    setValue(e.target.value)
+  }
+}
